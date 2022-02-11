ee5c219b8e051037ee2dcce98e0d4a67a19db614
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 20:59:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def hello_world():
     numb = random.randrange(1,2)
     #dictionary = {0 : 'Zero', 1 : 'One', 2 : 'Two', 3 : 'Three' }
     if numb == 20:
-        response = str(random.randrange(0,300) - 100) + '%'
-        return response
+        #response = str(random.randrange(0,300) - 100) + '%'
+        return numb #response
     else
-        return str(random.randrange(0,100)) + '%'
+        return numb #str(random.randrange(0,100)) + '%'
 
 if __name__ == '__main__':
     app.run()
