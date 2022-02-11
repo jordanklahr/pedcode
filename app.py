2f853bfe43bcb2b3bbff51662f1e8c35cdad34ee
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 20:04:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ from datetime import datetime, timedelta
 import time
 import json
 import os
-from trending import get_trending
+
 
 app = Flask(__name__)
 
 @app.route('/')
 
-def trending():
-   info = get_trending()
-   render_template('display.html', info=info['data'])
-   return res
 
 if __name__ == "__main__":
      app.debug = False
