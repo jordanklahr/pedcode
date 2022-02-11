ca34072a9c2e7cf835be724c16e60af45a8ca05d
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 20:14:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import flask
+from flask import Flask, render_template, redirect, url_for, request
 from datetime import datetime, timedelta
 import json
 import os
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
