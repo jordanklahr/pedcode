df321cf4ced97bb131cbd481ec6c47e652885b3d
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 21:31:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/love')
 def hello_world():
-    #numb = random.randrange(1,20)
-    numb = 20
+    numb = random.randint(1,20)
     #dictionary = {0 : 'Zero', 1 : 'One', 2 : 'Two', 3 : 'Three' }
     if numb == 20:
         response = str(random.randrange(0,300) - 100) + '%'
