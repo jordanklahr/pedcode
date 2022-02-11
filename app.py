3c6b2e54dce0fba8417717c283c2e88abf8d3601
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 20:53:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    numb = random.randrange(1,20)
+    #numb = random.randrange(1,20)
  #   dictionary = {0 : 'Zero', 1 : 'One', 2 : 'Two', 3 : 'Three' }
  #   if numb = 20:
 #        response = str(random.randrange(0,300) - 100) + '%'
  #       return response
  #   else
-    return numb.str()
+    return "test" #numb.str()
  #       return str(random.randrange(0,100)) + '%'
 
 if __name__ == '__main__':
