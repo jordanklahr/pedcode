a80f5d3ce071878bd743a0c3f96d3cc841c8d961
jordanklahr <31713419+jordanklahr@users.noreply.github.com>
2022-02-10 20:06:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,4 @@ app = Flask(__name__)
 
 
 if __name__ == "__main__":
-     app.debug = False
-     port = int(os.environ.get('PORT', 33507))
-     waitress.serve(app, port=port)
+     return 'Hello World!'
